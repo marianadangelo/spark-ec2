162e24f565d5a325091c30a088eacd15f27beba8
Sukwon Oh <soh@cs.utoronto.ca>
2015-03-10 14:50:02 -0400

    updated ganglia

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # NOTE: Remove all rrds which might be around from an earlier run
-sudo rm -rf /var/lib/ganglia/rrds/*
-sudo rm -rf /mnt/ganglia/rrds/*
+sudo sh -c 'rm -rf /var/lib/ganglia/rrds'
+sudo sh -c 'rm -rf /mnt/ganglia/rrds'
 
 # Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
-sudo rmdir /var/lib/ganglia/rrds
-sudo ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
-
-# Make sure rrd storage directory has right permissions
-sudo mkdir -p /mnt/ganglia/rrds
-sudo chown -R nobody:nogroup /mnt/ganglia/rrds
+sudo sh -c 'mkdir -p /mnt/ganglia/rrds'
+sudo sh -c 'chown -R nobody:nogroup /mnt/ganglia/rrds'
+sudo sh -c 'ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds'
 
 # Install ganglia
 # TODO: Remove this once the AMI has ganglia by default
@@ -18,12 +15,16 @@ sudo chown -R nobody:nogroup /mnt/ganglia/rrds
 #GANGLIA_PACKAGES="ganglia ganglia-web ganglia-gmond ganglia-gmetad"
 GANGLIA_PACKAGES="ganglia-monitor ganglia-webfrontend gmetad"
 
-#if ! rpm --quiet -q $GANGLIA_PACKAGES; then
-#  yum install -q -y $GANGLIA_PACKAGES;
-#fi
 sudo apt-get install -q -y $GANGLIA_PACKAGES
+
 for node in $SLAVES $OTHER_MASTERS; do
-  #ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
-  ssh -t -t $SSH_OPTS ubuntu@$node "sudo apt-get install -q -y $GANGLIA_PACKAGES" & sleep 0.3
+  ssh -t -t $SSH_OPTS ubuntu@$node "sudo sh -c 'rm -rf /var/lib/ganglia/rrds'" & sleep 0.3
+  ssh -t -t $SSH_OPTS ubuntu@$node "sudo sh -c 'rm -rf /mnt/ganglia/rrds'" & sleep 0.3
+
+  # Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
+  ssh -t -t $SSH_OPTS ubuntu@$node "sudo sh -c 'mkdir -p /mnt/ganglia/rrds'" & sleep 0.3
+  ssh -t -t $SSH_OPTS ubuntu@$node "sudo sh -c 'chown -R nobody:nogroup /mnt/ganglia/rrds'" & sleep 0.3
+  ssh -t -t $SSH_OPTS ubuntu@$node "sudo sh -c 'ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds'" & sleep 0.3
+  ssh -t -t $SSH_OPTS ubuntu@$node "sudo apt-get install -q -y ganglia-monitor" & sleep 0.3
 done
 wait
--- ganglia/setup.sh
+++ ganglia/setup.sh
@@ -3,18 +3,13 @@
 /home/ubuntu/spark-ec2/copy-dir /etc/ganglia/
 
 # Start gmond everywhere
-sudo /etc/init.d/ganglia-monitor restart
+sudo service ganglia-monitor restart
 
 for node in $SLAVES $OTHER_MASTERS; do
   ssh -t -t $SSH_OPTS ubuntu@$node "sudo /etc/init.d/ganglia-monitor restart"
 done
 
-# gmeta needs rrds to be owned by nobody
-sudo chown -R nobody /mnt/ganglia/rrds
-# cluster-wide aggregates only show up with this. TODO: Fix this cleanly ?
-sudo ln -s /usr/share/ganglia/conf/default.json /var/lib/ganglia-web/conf/
-
-sudo /etc/init.d/gmetad restart
+sudo service gmetad restart
 
 # Start http server to serve ganglia
-sudo /etc/init.d/apache2 restart
+sudo service apache2 restart
