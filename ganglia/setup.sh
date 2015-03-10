94599e6d49c26d5e8416f9ab744b8ed126f69df3
Sukwon Oh <soh@cs.utoronto.ca>
2015-03-10 15:03:33 -0400

    another fix

--- ganglia/setup.sh
+++ ganglia/setup.sh
@@ -6,8 +6,11 @@
 sudo service ganglia-monitor restart
 
 for node in $SLAVES $OTHER_MASTERS; do
+  ssh -t -t $SSH_OPTS ubuntu@$node "sudo chown -R nobody:nogroup /mnt/ganglia/rrds"
   ssh -t -t $SSH_OPTS ubuntu@$node "sudo /etc/init.d/ganglia-monitor restart"
 done
+
+sudo chown -R nobody:nogroup /mnt/ganglia/rrds
 
 sudo service gmetad restart
 
